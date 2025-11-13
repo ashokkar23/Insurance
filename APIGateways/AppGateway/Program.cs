8984aaef22eca4098187eeb14efb2cde1ef6cce3
ashokkar23 <ashok.kar23@gmail.com>
2025-11-13 15:07:04 +0530

    Gateway Implemted Sucessfully

--- APIGateways/AppGateway/Program.cs
+++ APIGateways/AppGateway/Program.cs
@@ -1,15 +1,30 @@
+using Microsoft.IdentityModel.Tokens;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 builder.Services.AddOcelot();
 
+var endPointAuthKey = builder.Configuration["Keys:EndPointAuthKey"];
+builder.Services.AddAuthentication().AddJwtBearer(endPointAuthKey, options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
+
 var app = builder.Build();
 
 app.UseOcelot().Wait();
-
-app.MapGet("/", () => "Hello World!");
 
 app.Run();
